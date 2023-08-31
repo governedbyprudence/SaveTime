9127ae4109672c6426a128bac8cb4a25e7890973
Punnyartha Banerjee <punnyarthabanerjee@gmail.com>
2023-08-31 15:31:33 +0530

    Added QR scan and show in list

--- lib/core/presentation/routes/qrScanPage.dart
+++ lib/core/presentation/routes/qrScanPage.dart
@@ -13,36 +13,69 @@ class _QRScanPageState extends State<QRScanPage> {
 
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   QRViewController? qrController;
+  ScrollController listScrollController = ScrollController();
+  List<TextEditingController> textControllers =[];
   List<String> qrData = [];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text("Scan QR codes"),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(20),
-        height: double.infinity,
-        width: double.infinity,
-        child: Column(
-          children: [
-            Expanded(child: _qrScanner()),
-            Expanded(flex: 4,child: _list())
-          ],
+    return GestureDetector(
+      onTap: (){
+        FocusManager.instance.primaryFocus!.unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Scan QR codes"),
+        ),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(30),
+          child: ElevatedButton(
+            onPressed: _save,
+            child:const Text("Save"),
+          ),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(20),
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+            children: [
+             // Expanded(child: _qrScanner()),
+              Expanded(child: _list())
+            ],
+          ),
         ),
       ),
     );
   }
-
+  void _save(){
+    Navigator.pop(context);
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        content:Text("Data saved",style: TextStyle(color: Colors.grey[900]),)));
+  }
   void _onQRViewCreated(QRViewController controller) {
     qrController = controller;
-    controller.scannedDataStream.listen((scanData) {
+    controller.scannedDataStream.listen((scanData) async{
       if(scanData.code!=null){
-        setState(() {
-          qrData.add(scanData.code!);
-        });
+        if(!qrData.contains(scanData.code)){
+          setState(() {
+            qrData.add(scanData.code!);
+            textControllers.add(TextEditingController());
+            listScrollController.animateTo(
+              listScrollController.position.maxScrollExtent,
+              duration: const Duration(seconds: 2),
+              curve: Curves.fastOutSlowIn,
+            );
+          });
+        }
+        await controller.pauseCamera();
+        Future.delayed(
+          const Duration(seconds: 4),
+            (){
+              controller.resumeCamera();
+            }
+        );
       }
     });
   }
@@ -59,19 +92,71 @@ class _QRScanPageState extends State<QRScanPage> {
   }
   Widget _list(){
     return ListView.builder(
+        controller: listScrollController,
         itemCount: qrData.length,
         itemBuilder: (context,index)=>Container(
-          margin: const EdgeInsets.all(20),
-          padding: const EdgeInsets.all(10),
-          child: Row(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.white,
+            boxShadow: const [
+              BoxShadow(
+                offset: Offset(0,1),
+                color: Colors.grey,
+                spreadRadius: 1,
+                blurRadius: 1
+              )
+            ]
+          ),
+          margin: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Expanded(child: TextFormField()),
-              Expanded(child: Text(qrData[index])),
-              Expanded(child: IconButton(onPressed: (){
-                  setState(() {
-                    qrData.removeAt(index);
-                  });
-              },icon: const Icon(Icons.delete),)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text((index+1).toString()),
+                  const SizedBox(width: 20,),
+                  Expanded(
+                    child: SizedBox(
+                      height: 40,
+                      child: TextFormField(
+                        controller: textControllers[index],
+                        decoration: InputDecoration(
+                            labelText: "Enter Label for QR",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15)
+                          )
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(width: 20,),
+                  IconButton(onPressed: (){
+                    setState(() {
+                      qrData.removeAt(index);
+                    });
+                  },icon: const Icon(Icons.delete),),
+                ],
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Theme(
+                      data: ThemeData().copyWith(dividerColor: Colors.transparent),
+                      child: ExpansionTile(title: const Text("Scanned Data"),
+
+                        children: [
+                        Container(
+                            alignment:Alignment.centerLeft,
+                            child: Text(qrData[index])),
+                      ],),
+                    ),
+                  ),
+                ],
+              )
+
             ],
           ),
         ));
